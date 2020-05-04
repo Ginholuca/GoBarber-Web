b5693f99559840759ee1e9b4cf4dec19c434133e
Giovanni De Luca <ginholucacorrea@icloud.com>
2020-05-04 02:58:37 -0300

    web

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
-import SignIn from './pages/SignIn'
-// import SignUp from './pages/SignUp'
 import GlobalStyle from './styles/global'
 
-import { AuthProvider } from './hooks/AuthContext'
+import AppProvider from './hooks'
+
+import Routes from './routes'
 
 const App: React.FC = () => (
-  <>
-    <AuthProvider>
-      <SignIn />
-    </AuthProvider>
+  <BrowserRouter>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
 
     <GlobalStyle />
-  </>
+  </BrowserRouter>
 )
 
 export default App
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,6 +3,11 @@ import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
+import { Link, useHistory } from 'react-router-dom'
+
+import api from '../../services/api'
+
+import { useToast } from '../../hooks/toast'
 
 import getValidationErrors from '../../utils/getValidationErrors'
 
@@ -11,64 +16,94 @@ import logoImg from '../../assets/logo.svg'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
-import { Container, Content, Background } from './styles'
+import { Container, Content, AnimationContainer, Background } from './styles'
+
+interface SignUpFormData {
+  name: string
+  email: string
+  password: string
+}
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
+  const { addToast } = useToast()
+  const history = useHistory()
 
   console.log(formRef)
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      /* eslint-disable no-unused-expressions */
-      formRef.current?.setErrors({})
+  const handleSubmit = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        /* eslint-disable no-unused-expressions */
+        formRef.current?.setErrors({})
 
-      const schema = Yup.object().shape({
-        name: Yup.string().required('Nome obrigatório'),
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
-      })
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        })
 
-      await schema.validate(data, {
-        abortEarly: false,
-      })
-    } catch (err) {
-      console.log(err)
+        await schema.validate(data, {
+          abortEarly: false,
+        })
 
-      const errors = getValidationErrors(err)
+        await api.post('/users', data)
 
-      formRef.current?.setErrors(errors)
-    }
-  }, [])
+        history.push('/')
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro realizado!',
+          description: 'Você já pode fazer seu logon no GoBarber!',
+        })
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err)
+
+          formRef.current?.setErrors(errors)
+
+          return
+        }
+        addToast({
+          type: 'error',
+          title: 'Erro no Cadastro.',
+          description: 'Ocorreu um erro ao fazer cadastro, tente novamente.',
+        })
+      }
+    },
+    [addToast, history]
+  )
 
   return (
     <Container>
       <Background />
 
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu cadastro</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
 
-          <Input name="name" icon={FiUser} placeholder="Nome" />
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Senha"
-          />
+            <Input name="name" icon={FiUser} placeholder="Nome" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
 
-          <Button type="submit">Cadastrar</Button>
-        </Form>
+            <Button type="submit">Cadastrar</Button>
+          </Form>
 
-        <a href="login">
-          <FiArrowLeft />
-          Voltar para logon
-        </a>
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar para logon
+          </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   )
